07d437614a16fa7ccaa8037aa9ac3cf9e720445d
sokari <sokariharry@gmail.com>
2024-04-09 03:03:09 +0100

    This is where it begins, my first attempt to launch a self funded project since Nearbuy.ng

---
+++ src/main/java/co/gatedaccess/web/GatedAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package co.gatedaccess.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatedAccessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatedAccessServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/gatedaccess/web/GatedAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.gatedaccess.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatedAccessServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
